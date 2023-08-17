f2f56792d71198c3b1179f91e493310498bde52d
arazo <jo91kr@gmail.com>
2023-08-18 00:50:22 +0900

    :white_check_mark: Flexible 연습

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -10,6 +10,36 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Column(
+        children: [
+          // UI 비율에 기반해서 더 유연하게 만들 수 있음
+          Flexible(
+            flex: 1,
+            child: Container(
+              decoration: const BoxDecoration(
+                color: Colors.red,
+              ),
+            ),
+          ),
+          Flexible(
+            flex: 2,
+            child: Container(
+              decoration: const BoxDecoration(
+                color: Colors.green,
+              ),
+            ),
+          ),
+          Flexible(
+            flex: 1,
+            child: Container(
+              decoration: const BoxDecoration(
+                color: Colors.blue,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
